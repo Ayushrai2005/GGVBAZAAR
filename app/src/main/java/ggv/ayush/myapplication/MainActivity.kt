b0dc82acfd3d1267e5f0d4558d9991329419d2bc
Ayushrai2005 <shweetarai@gmail.com>
2024-03-07 16:40:24 +0530

    Initial commit , Created Login , sinup , reset page ui , later implement Backend into it.

---
+++ app/src/main/java/ggv/ayush/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package ggv.ayush.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ggv.ayush.myapplication.ui.theme.GGVBAZAARTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GGVBAZAARTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainView()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ggv/ayush/myapplication/MainView.kt
@@ -0,0 +1,27 @@
+package ggv.ayush.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ggv.ayush.myapplication.LOGINSIGNUP.LoginPage
+import ggv.ayush.myapplication.LOGINSIGNUP.RegisterPage
+import ggv.ayush.myapplication.LOGINSIGNUP.ResetPage
+
+@Composable
+fun MainView(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login_page", builder = {
+
+
+
+        composable("login_page", content = { LoginPage(navController = navController) })
+
+        composable("register_page", content = { RegisterPage(navController = navController) })
+
+        composable("reset_page", content = { ResetPage(navController = navController) })
+
+
+    })
+}
